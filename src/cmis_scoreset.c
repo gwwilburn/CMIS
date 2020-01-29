58c1f45deff5a55d95d68d23e55e5195a16eb1bc
gwwilburn <wilburn@g.harvard.edu>
2020-01-29 16:08:30 -0500

    modified cmis_scoreset to handle bitscore and sequence hit bounds from RefFInsideScan()

--- src/cmis_scoreset.c
+++ src/cmis_scoreset.c
@@ -22,19 +22,25 @@ cmis_scoreset_Create(int nseq) {
    ESL_ALLOC(cmis_ss->sqname, sizeof(char *) * nseq);
 
    /* allocate memory for probabilities/scores */
-   ESL_ALLOC(cmis_ss->R,           sizeof(int)   * nseq);
-   ESL_ALLOC(cmis_ss->fsc,         sizeof(float) * nseq);
-   ESL_ALLOC(cmis_ss->ntsc,        sizeof(float) * nseq);
-   ESL_ALLOC(cmis_ss->insc,        sizeof(float) * nseq);
-   ESL_ALLOC(cmis_ss->cmis_ld,     sizeof(float) * nseq);
+   ESL_ALLOC(cmis_ss->R,           sizeof(int)     * nseq);
+   ESL_ALLOC(cmis_ss->fsc,         sizeof(float)   * nseq);
+   ESL_ALLOC(cmis_ss->ntsc,        sizeof(float)   * nseq);
+   ESL_ALLOC(cmis_ss->insc,        sizeof(float)   * nseq);
+   ESL_ALLOC(cmis_ss->scansc,      sizeof(float)   * nseq);
+   ESL_ALLOC(cmis_ss->seq_from,    sizeof(int)     * nseq);
+   ESL_ALLOC(cmis_ss->seq_to,      sizeof(int)     * nseq);
+   ESL_ALLOC(cmis_ss->cmis_ld,     sizeof(float)   * nseq);
 
 
    /* initialize values */
-   esl_vec_ISet(cmis_ss->R,        nseq,   0);
-   esl_vec_FSet(cmis_ss->fsc,      nseq, 0.0);
-   esl_vec_FSet(cmis_ss->ntsc,     nseq, 0.0);
-   esl_vec_FSet(cmis_ss->insc,     nseq, 0.0);
-   esl_vec_FSet(cmis_ss->cmis_ld,  nseq, 0.0);
+   esl_vec_ISet(cmis_ss->R,         nseq,   0);
+   esl_vec_FSet(cmis_ss->fsc,       nseq, 0.0);
+   esl_vec_FSet(cmis_ss->ntsc,      nseq, 0.0);
+   esl_vec_FSet(cmis_ss->insc,      nseq, 0.0);
+   esl_vec_FSet(cmis_ss->scansc,    nseq, 0.0);
+   esl_vec_ISet(cmis_ss->seq_from,  nseq,   0);
+   esl_vec_ISet(cmis_ss->seq_to,    nseq,   0);
+   esl_vec_FSet(cmis_ss->cmis_ld,   nseq, 0.0);
 
    return cmis_ss;
 
@@ -50,12 +56,15 @@ cmis_scoreset_Destroy( CMIS_SCORESET *cmis_ss) {
    if (cmis_ss == NULL) return;
 
    /* free arrays */
-   if (cmis_ss->sqname)  free(cmis_ss->sqname);
-   if (cmis_ss->R)       free(cmis_ss->R);
-   if (cmis_ss->fsc)     free(cmis_ss->fsc);
-   if (cmis_ss->ntsc)    free(cmis_ss->ntsc);
-   if (cmis_ss->insc)    free(cmis_ss->insc);
-   if (cmis_ss->cmis_ld) free(cmis_ss->cmis_ld);
+   if (cmis_ss->sqname)    free(cmis_ss->sqname);
+   if (cmis_ss->R)         free(cmis_ss->R);
+   if (cmis_ss->fsc)       free(cmis_ss->fsc);
+   if (cmis_ss->ntsc)      free(cmis_ss->ntsc);
+   if (cmis_ss->insc)      free(cmis_ss->insc);
+   if (cmis_ss->scansc)    free(cmis_ss->scansc);
+   if (cmis_ss->seq_from)  free(cmis_ss->seq_from);
+   if (cmis_ss->seq_to)    free(cmis_ss->seq_to);
+   if (cmis_ss->cmis_ld)   free(cmis_ss->cmis_ld);
 
    /* free the scoreset object */
    free(cmis_ss);
@@ -68,16 +77,19 @@ cmis_scoreset_Write(FILE *fp, CMIS_SCORESET *cmis_ss) {
    int n;
 
    /* write csv header line */
-   fprintf(fp, "id,N_sample,raw_fwd,null_tran,inside_logodds,cmis_logodds\n");
+   fprintf(fp, "id,N_sample,raw_fwd,null_tran,InsideAlign_logodds,InsideScan_logodds,seq_from,seq_to,cmis_logodds\n");
 
    /* loop over sequences, print id and scores */
    for (n = 0; n < cmis_ss->nseq; n++) {
-      fprintf(fp, "%s,%d,%.4f,%.4f,%.4f,%.4f\n",
+      fprintf(fp, "%s,%d,%.4f,%.4f,%.4f,%.4f,%d,%d,%.4f\n",
                   cmis_ss->sqname[n],
                   cmis_ss->R[n],
                   cmis_ss->fsc[n],
                   cmis_ss->ntsc[n],
                   cmis_ss->insc[n],
+                  cmis_ss->scansc[n],
+                  cmis_ss->seq_from[n],
+                  cmis_ss->seq_to[n],
                   cmis_ss->cmis_ld[n]);
     }
 
--- src/cmis_scoreset.h
+++ src/cmis_scoreset.h
@@ -7,13 +7,16 @@
 #include "esl_vectorops.h"
 
 typedef struct cmisscoreset_s{
-   char   **sqname;  /* sequence names, [0...nseq-1][], \0-terminated                 */
-   int      nseq;    /* number of sequences                                           */
-   int     *R;       /* number of samples/sequence, [0...nseq-1]                      */
-   float   *fsc;     /* forward partial log-odds scores, in bits [0...nseq-1]         */
-   float   *ntsc;    /* hmmer null model transition scores, in bits [0...nseq-1]      */
-   float   *insc;    /* inside alg log odds scores, in bits, [0...nseq-1]             */
-   float   *cmis_ld; /* CM importance sampling log-odds scores, in bits, [0...nseq-1] */
+   char   **sqname;    /* sequence names, [0...nseq-1][], \0-terminated                 */
+   int      nseq;      /* number of sequences                                           */
+   int     *R;         /* number of samples/sequence, [0...nseq-1]                      */
+   float   *fsc;       /* forward partial log-odds scores, in bits [0...nseq-1]         */
+   float   *ntsc;      /* hmmer null model transition scores, in bits [0...nseq-1]      */
+   float   *insc;      /* inside align alg log odds scores, in bits, [0...nseq-1]       */
+   float   *scansc;    /* inside scan alg log odds scores, in bits, [0...nseq-1]        */
+   int     *seq_from;  /* start point of inside scan hit, in seq coords                 */
+   int     *seq_to;    /* end point of inside scan hit, in seq coords                   */
+   float   *cmis_ld;   /* CM importance sampling log-odds scores, in bits, [0...nseq-1] */
 
 } CMIS_SCORESET;
 
